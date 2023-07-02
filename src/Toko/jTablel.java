a68eb9c7f8d82d5dd6200440cf93c4587ad6e7ba
iswanto123 <iswantoir123@gmail.com>
2023-07-02 06:05:54 -0800

    first commit

---
+++ src/Toko/jTablel.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Toko;
+
+/**
+ *
+ * @author user
+ */
+class jTablel {
+
+    static String getValueAt;
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Object getValueAt(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
